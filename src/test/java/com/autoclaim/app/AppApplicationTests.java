22e6f73904ddb061949686137473fd7c3442492f
ZGhali <ZGhali@win.cccis.com>
2023-04-13 18:20:15 -0500

    Add existing project files to Git

---
+++ src/test/java/com/autoclaim/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoclaim.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
